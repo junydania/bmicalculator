3023fa08e93284803874f7fd824e96d382960ae1
junydania <o.dania@aol.com>
2017-04-11 10:08:17 +0200

    initial Commit

---
+++ src/bmi_calculator.js
@@ -0,0 +1,30 @@
+function BMICalculator() {
+
+}
+BMICalculator.prototype.metric_bmi = function(obj) {
+  var weight = obj.weight;
+  var height = obj.height;
+
+  if (weight > 0 && height > 0) {
+    var finalBMI = weight / (height /100 * height / 100);
+    obj.bmiValue = parseFloat(finalBMI.toFixed(2));
+  }
+};
+
+function setBMIMessage (obj) {
+  if (obj.bmiValue < 18.5) {
+    obj.bmiMessage = "Underweight";
+  }
+
+  else if (obj.bmiValue > 18.5 && obj.bmiValue < 25){
+    obj.bmiMessage = "Normal";
+  }
+
+  else if(obj.bmiValue > 25 && obj.bmiValue < 30) {
+    obj.bmiMessage = "Overweight";
+  }
+  else if (obj.bmiValue > 30 && obj.bmiValue < 60) {
+    obj.bmiMessage = "Obese";
+  }
+  else { obj.bmiMessage = "You have exceeded obese and need to see a doctor";}
+}
---
+++ src/person.js
@@ -0,0 +1,11 @@
+function Person(attr) {
+  this.weight = attr.weight;
+  this.height = attr.height;
+}
+
+Person.prototype.calculate_bmi = function() {
+  calculator = new BMICalculator();
+  calculator.metric_bmi(this);
+//  this.bmiValue = 26.01;
+// this.bmiMessage = "Overweight";
+};
